a999c960736537eb13edec7f281bba474b229178
apaixav <lucasrib01@gmail.com>
2023-10-24 20:05:46 -0300

    layout

---
+++ layout_guia/src/App.tsx
@@ -0,0 +1,16 @@
+//sempre que eu quiser que algo seja exibido, eu colo aqui
+//import Card from "./components/card/Card"
+//import Contador from "./components/contador/Contador"
+//import Tarefa from "./components/tarefa/Tarefa"
+
+import Home from "./pages/home/Home"
+
+function App() {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default App
